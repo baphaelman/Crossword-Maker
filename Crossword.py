eb7409db9224d47150c430dac472de03044d60ca
Steven <steven.sun@berkeley.edu>
2024-10-19 14:04:49 -0700

    generate board

--- Crossword.py
+++ Crossword.py
@@ -26,4 +26,38 @@ class Crossword:
     def valid_board(self):
         return self.valid_rows() and self.valid_columns()
     
-    
+    def generate_board(self, word):
+        board = self.board
+        #word is down
+        for i in range (1, len(board.cols)):
+            for j in range (1, len(board.rows) - len(word)):
+                valid = True
+                for k in range (0, len(word)):
+                    if (board.get(j + k,i)) != word[k] and board.get(j + k, i) != 0:
+                        valid = False
+                if valid:
+                    new_board = board.clone()
+                    for k in range (0, len(word)):
+                        new_board.insert(j + k, i , word[k])
+                        if new_board.isValid():
+                            yield new_board
+                        
+        #word is across
+        for i in range (1, len(board.rows)):
+            for j in range (1, len(board.cols) - len(word)):
+                valid = True
+                for k in range (0, len(word)):
+                    if (board.get(i, j + k)) != word[k] and board.get(i, j + k) != 0:
+                        valid = False
+                if valid:
+                    new_board = board.clone()
+                    for k in range (0, len(word)):
+                        new_board.insert(i,  j + k , word[k])
+                        if new_board.isValid():
+                            yield new_board
+        
+
+
+
+
+
