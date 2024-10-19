3c3850ffd4995c0d8bbd24848a15f504d3d576a2
Raphael Bajet <baphael@berkeley.edu>
2024-10-19 10:49:09 -0700

    init works

--- Crossword.py
+++ Crossword.py
@@ -5,10 +5,11 @@ class Crossword:
         for col in range(size + 2):
             row_builder = []
             for row in range(size + 2):
-                if row == 0 or row == size + 1:
+                if col == 0 or col == size + 1:
                     row_builder.append("#")
-                elif col == 0 or col == size + 1:
+                elif row == 0 or row == size + 1:
                     row_builder.append("#")
-                row_builder.append(None)
+                else:
+                    row_builder.append(0)
             self.board.append(row_builder)
         print(self.board)
