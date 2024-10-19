f6c2151454f01cacf2ec7b758c726a4f3a7735aa
Raphael Bajet <baphael@berkeley.edu>
2024-10-19 10:58:38 -0700

    added repre for printing

--- Crossword.py
+++ Crossword.py
@@ -1,6 +1,15 @@
+from typing import List
+
 class Crossword:
-    def __init__(self, size):
+    # self.size: size of crossword (ex. 5 -> 5x5)
+    # self.board: list of lists. size + 2 for hashtages along border
+    # self.words: list of words the user wants to include in the 
+
+    def __init__(self, size: int, words: List[str]):
         self.size = size
+        self.words = words
+
+        # initialize board with # along border, 0 in the center
         self.board = []
         for col in range(size + 2):
             row_builder = []
@@ -13,3 +22,13 @@ class Crossword:
                     row_builder.append(0)
             self.board.append(row_builder)
         print(self.board)
+    
+    def __repr__(self):
+        rowString = ""
+        for row in self.board:
+            for item in row:
+                rowString += str(item)
+                rowString += " "
+            rowString += "\n"
+            
+        return(rowString)
