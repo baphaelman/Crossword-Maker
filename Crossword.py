065c15cfaa9ff01c3bb903f4131b2bbacb180bcb
Raphael Bajet <baphael@berkeley.edu>
2024-10-19 10:46:45 -0700

    basic crosswrod logic

--- Crossword.py
+++ Crossword.py
@@ -2,5 +2,13 @@ class Crossword:
     def __init__(self, size):
         self.size = size
         self.board = []
-        for _ in range(size + 2):
-
+        for col in range(size + 2):
+            row_builder = []
+            for row in range(size + 2):
+                if row == 0 or row == size + 1:
+                    row_builder.append("#")
+                elif col == 0 or col == size + 1:
+                    row_builder.append("#")
+                row_builder.append(None)
+            self.board.append(row_builder)
+        print(self.board)
--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+from Crossword import Crossword
+
+def main() -> None:
+    crossword = Crossword(3)
+
 if __name__ == "__main__":
     main()
